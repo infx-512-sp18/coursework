920ecda5240a69ceff6d6a4c8d6bf979c2c65daf
Nick DeNardis <nick.denardis@gmail.com>
2018-03-05 20:36:41 -0500

    Getting setup complete

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,14 @@ module.exports = {
     link: [
       {
         rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'
+      },
+      {
+        rel: 'stylesheet', href: '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css'
+      }
+    ],
+    script: [
+      {
+        src: '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js'
       }
     ]
   },
--- pages/getting-setup.vue
+++ pages/getting-setup.vue
@@ -2,48 +2,52 @@
   <main id="main" tabindex="-1">
     <h1>Getting setup tutorial</h1>
     <p>Before you get started with this course, you need to install a few tools and create a few accounts. All of these things are free and available on all major platforms (Windows, OS X, and Linux).</p>
+
+    <h2>Sign up</h2>
+    <ul>
+      <li>
+        <a href="https://github.com/join">GitHub</a> - We will store all code in GitHub. There is also a
+        <a href="https://education.github.com/">Student Developer Pack</a> which gives you unlimited private repositories and other useful resources.
+      </li>
+    </ul>
+
     <h2>Required tools</h2>
     <ul>
       <li>
-        <a href="https://slack.com/downloads">Slack</a>
+        <a href="https://slack.com/downloads">Slack</a> - A group chat tool that we will use for daily communication. Slack is available as a Web app, desktop and mobile application. The desktop application is recommended.
       </li>
       <li>
-        <a href="https://desktop.github.com/">GitHub Desktop</a>
+        <a href="https://desktop.github.com/">GitHub Desktop</a> - This tool makes working with GIT visual.
       </li>
       <li>
-        <a href="https://code.visualstudio.com/">Visual Studio Code</a>
+        <a href="https://code.visualstudio.com/">Visual Studio Code</a> - Code editor we will be using throughout the course.
       </li>
       <li>
-        <a href="https://www.google.com/chrome/">Google Chrome</a>
+        <a href="https://www.google.com/chrome/">Google Chrome</a> - Web browser with developer tools that will allow us to inspect Web pages and javascript.
       </li>
     </ul>
 
     <h2>Command line tools</h2>
     <ul>
       <li>
-        <a href="https://nodejs.org/en/download/">Node JS and NPM</a>
+        <a href="https://nodejs.org/en/download/">Node JS and NPM</a> - These tools will help us build modern Web pages.
       </li>
       <li>
-        <a href="https://www.browsersync.io/">BrowserSync</a>
+        <a href="https://www.browsersync.io/">BrowserSync</a> - A tool used to watch files for updates and allow live previewing of Web pages.
       </li>
     </ul>
 
     <p>These tools can be downloaded from the website or installed via
       <a href="https://brew.sh/">Homebrew</a> (MacOS only) with the following commands:</p>
     <p>
-      <pre>
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        brew install node
-        npm install -g browser-sync
+      <pre class="text-sm m-0 p-0">
+        <code class="shell">
+$ /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+$ brew install node
+$ npm install -g browser-sync
+        </code>
       </pre>
     </p>
-
-    <h2>Sign up</h2>
-    <ul>
-      <li>
-        <a href="https://github.com/join">GitHub</a>
-      </li>
-    </ul>
   </main>
 </template>
 
