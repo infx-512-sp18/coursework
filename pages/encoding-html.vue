937e3428f12b9dd73db010b5d3181cb45f379c03
Nick DeNardis <nick.denardis@gmail.com>
2018-03-11 14:09:09 -0400

    Getting setup mostly done

--- pages/encoding-html.vue
+++ pages/encoding-html.vue
@@ -1,6 +1,43 @@
 <template>
   <main id="main" tabindex="-1">
     <h1>Encoding in HTML</h1>
+
+    <h2>History of HTML</h2>
+
+    <ul>
+      <li>Invented by Tim Berners-Lee</li>
+      <li>Created "hypertext" to share scientific papers</li>
+      <li>First web page August 6, 1991</li>
+      <li>Standardized by w3 Consortium</li>
+    </ul>
+
+    <ul>
+      <li>HyperText Markup Language</li>
+      <li>Early 90s</li>
+      <li>HTML 4 in 1997</li>
+      <li>XHTML in 2000</li>
+      <li>HTML 5 in 2014</li>
+    </ul>
+
+    <h2>Terms</h2>
+
+    <ul>
+      <li>
+        <strong>Web design</strong><br> The process of planning, structuring and creating a website</li>
+      <li>
+        <strong>Web development</strong><br> The process of programming dynamic web applications</li>
+      <li>
+        <strong>Front end</strong><br> The outwardly visible elements of a website or application</li>
+      <li>
+        <strong>Back end</strong><br> The inner workings and functionality of a website or application.</li>
+    </ul>
+
+    <h2>Folder structure</h2>
+
+    <p>File names are case sensitive and should not include spaces or special characters.</p>
+
+    <p><img src="~/assets/encoding-html/html-folder-structure.png" alt="Screenshot of HTML folder structure"></p>
+
   </main>
 </template>
 
--- pages/getting-setup.vue
+++ pages/getting-setup.vue
@@ -32,18 +32,42 @@
       <li>
         <a href="https://nodejs.org/en/download/">Node JS and NPM</a> - These tools will help us build modern Web pages.
       </li>
-      <li>
-        <a href="https://www.browsersync.io/">BrowserSync</a> - A tool used to watch files for updates and allow live previewing of Web pages.
-      </li>
     </ul>
 
     <p>These tools can be downloaded from the website or installed via
       <a href="https://brew.sh/">Homebrew</a> (MacOS only) with the following commands:</p>
 
     <pre class="text-sm m-0 p-0"><code class="language-bash">$ /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-$ brew install node
-$ npm install -g browser-sync</code></pre>
+$ brew cask install google-chrome slack visual-studio-code github
+$ brew install node</code></pre>
 
+    <h2>Customizing VS Code</h2>
+
+    <p>These extensions will help you as a developer get the most out of Visual Studio Code.</p>
+
+    <ul>
+      <li>
+        <a href="https://marketplace.visualstudio.com/items?itemName=shinnn.stylelint">StyleLint</a>
+      </li>
+      <li>
+        <a href="https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode">Prettier</a>
+      </li>
+      <li>
+        <a href="https://marketplace.visualstudio.com/items?itemName=christian-kohler.path-intellisense">Path Intellisense</a>
+      </li>
+      <li>
+        <a href="https://marketplace.visualstudio.com/items?itemName=Zignd.html-css-class-completion">IntelliSense for CSS class names</a>
+      </li>
+      <li>
+        <a href="https://marketplace.visualstudio.com/items?itemName=pranaygp.vscode-css-peek">CSS Peek</a>
+      </li>
+      <li>
+        <a href="https://marketplace.visualstudio.com/items?itemName=formulahendry.auto-close-tag">Auto close tag</a>
+      </li>
+      <li>
+        <a href="https://marketplace.visualstudio.com/items?itemName=formulahendry.auto-rename-tag">Auto rename tag</a>
+      </li>
+    </ul>
   </main>
 </template>
 
